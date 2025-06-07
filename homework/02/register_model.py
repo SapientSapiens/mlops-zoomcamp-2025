48b0b7ed596b1c8c5f0db3e2136a4d2c653aa95e
SapientSapiens <siddharthagogoi@gmail.com>
2025-06-07 23:52:54 +0530

    rectifying overlooked mistake: register only lowest test_rmse model to registry

--- homework/02/register_model.py
+++ homework/02/register_model.py
@@ -76,7 +76,8 @@ def run_register_model(data_path: str, top_n: int):
     best_run = client.search_runs(  
         experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
-        order_by=["metrics.rmse ASC"]  )[0]
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
 
     # Register the best model
     model_uri = f"runs:/{best_run.info.run_id}/model"
